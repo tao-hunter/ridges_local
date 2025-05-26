c0b53ffb81d866f26e9466bfea1a2016111c5e69
Shakeel Hussein <shakeel.hussein@mail.utoronto.ca>
2025-05-26 14:40:50 -0400

    create miner main loop and endpoints

---
+++ miner/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fiber.logging_utils import get_logger
+
+from miner.dependancies import get_config, Config
+from miner.endpoints.codegen import router as codegen_router
+from miner.endpoints.availability import router as availability_router
+
+logger = get_logger(__name__)
+
+app = FastAPI()
+
+app.dependency_overrides[Config] = get_config()
+
+# Include relevant miner routers 
+app.include_router(
+    codegen_router, 
+    prefix="/codegen", 
+    tags=["codegen"]
+)
+
+app.include_router(
+    availability_router,
+    tags=["availability"]
+)
