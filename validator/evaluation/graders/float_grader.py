668085ce12c0450e2ba15478c8369703f2c7c36a
Shakeel Hussein <shakeel.hussein@mail.utoronto.ca>
2025-05-27 17:28:17 -0400

    move to only elo grader

--- validator/evaluation/graders/float_grader.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from validator.challenge.challenge_types import CodegenResponse, GeneratedCodegenProblem, ValidationResult
-from .abstract_grader import GraderInterface
-
-class FloatGrader(GraderInterface):
-    def grade(self, problem: GeneratedCodegenProblem, responses: List[CodegenResponse]) -> List[ValidationResult]:
-        return []
