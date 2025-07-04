856db6f95c784d01d0551900b273493bcf8804d2
Aaron Olsen <aolsen@uwaterloo.ca>
2025-07-04 00:27:00 -0400

    Use asyncio boto3

--- api/src/db/s3.py
+++ api/src/db/s3.py
@@ -1,4 +1,4 @@
-import boto3
+import aioboto3
 import os
 from typing import BinaryIO
 from dotenv import load_dotenv
@@ -9,15 +9,18 @@ s3_bucket_name = os.getenv('AWS_S3_BUCKET_NAME')
 
 class S3Manager:
     def __init__(self):
-        self.s3 = boto3.client('s3')
+        self.session = aioboto3.Session()
     
-    def upload_file_object(self, file_object: BinaryIO, key: str):
-        self.s3.upload_fileobj(file_object, s3_bucket_name, key)
+    async def upload_file_object(self, file_object: BinaryIO, key: str):
+        async with self.session.client('s3') as s3:
+            await s3.upload_fileobj(file_object, s3_bucket_name, key)
 
-    def get_file_text(self, key: str) -> str:
-        response = self.s3.get_object(Bucket=s3_bucket_name, Key=key)
-        return response['Body'].read().decode('utf-8')
+    async def get_file_text(self, key: str) -> str:
+        async with self.session.client('s3') as s3:
+            response = await s3.get_object(Bucket=s3_bucket_name, Key=key)
+            return (await response['Body'].read()).decode('utf-8')
     
-    def get_file_object(self, key: str) -> BinaryIO:
-        response = self.s3.get_object(Bucket=s3_bucket_name, Key=key)
-        return response['Body']
+    async def get_file_object(self, key: str) -> BinaryIO:
+        async with self.session.client('s3') as s3:
+            response = await s3.get_object(Bucket=s3_bucket_name, Key=key)
+            return response['Body']
