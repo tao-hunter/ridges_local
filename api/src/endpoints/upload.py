ac78e681c07ababb4ba00671931cdd4b666bc1bd
Shakeel Hussein <shakeel.hussein@mail.utoronto.ca>
2025-07-06 10:13:16 -0400

    more bans

--- api/src/endpoints/upload.py
+++ api/src/endpoints/upload.py
@@ -38,7 +38,8 @@ banned_hotkeys = [
     "5GBsbxyQvs78dDJj8p1qMjUYTdQGpAeKksfbHMn5HenntzGA",
     "5Dy33595c9dqwtyXm4CYHu4bfGNYgVGY34ARbktNNgLR4MBQ", 
     "5EL5k31Wm9N74WbUG7SwTCHCbD31ERGH1yBmFsLRtvGjvtvN",
-    "5CwU1yR9SXiayopaHPNSU7ony5A1xteyd4S88cNZcys8Uzsu"
+    "5CwU1yR9SXiayopaHPNSU7ony5A1xteyd4S88cNZcys8Uzsu",
+    "5HCJjSzoraw8VKHpvpstGCExxNWzuG8hLW54rvFABZtnHjz2"
 ]
 
 async def post_agent(
