0c528f18a46efb114442722b190f18ea87769561
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-07-11 12:45:42 -0400

    update top agent code on startup

--- api/src/main.py
+++ api/src/main.py
@@ -19,6 +19,7 @@ from api.src.endpoints.retrieval import router as retrieval_router
 from api.src.endpoints.scoring import router as scoring_router, run_weight_setting_loop
 from api.src.socket.websocket_manager import WebSocketManager
 from api.src.endpoints.healthcheck import router as healthcheck_router
+from api.src.utils.top_agent_code import update_top_agent_code
 
 logger = get_logger(__name__)
 
@@ -32,6 +33,7 @@ async def lifespan(app: FastAPI):
     _batch_task = asyncio.create_task(batch_writer(app.state.stop_event, queue))
 
     await clean_hanging_evaluations()
+    await update_top_agent_code()
     asyncio.create_task(run_weight_setting_loop(30))
     asyncio.create_task(evaluation_cleanup_loop(timedelta(minutes=10)))
     # asyncio.create_task(run_weight_monitor(netuid=62, interval_seconds=60))
---
+++ api/src/utils/top_agent_code.py
@@ -0,0 +1,29 @@
+from api.src.backend.queries.statistics import get_top_agents
+from api.src.utils.s3 import S3Manager
+from api.src.utils.logging_utils import get_logger
+
+s3 = S3Manager()
+
+logger = get_logger(__name__)
+
+async def update_top_agent_code():
+    """
+    Updates the top agent code in the miner/agent.py file.
+    """
+    # Get top agent versionid
+    top_agents = await get_top_agents(num_agents=1)
+    top_agent = top_agents[0]
+    logger.info(f"Updating top agent code with version id {top_agent.version_id}.")
+
+    # Get code text from s3
+    code_text = await s3.get_file_text(f"53d2937f-f851-4fe4-b620-7e4199b1f184/agent.py")
+    logger.info(f"Got code text from s3 for version id {top_agent.version_id}.")
+
+    # Append warning on the top
+    code_text = f"# This is the top agent. We encourage you to try to improve it, however purely copying and submitting it will result in an error.\n{code_text}"
+
+    # Replace miner/agent.py with the new code
+    with open("miner/agent.py", "w") as f:
+        f.write(code_text)
+
+    logger.info(f"Successfully updated top agent code with version id {top_agent.version_id}.")
