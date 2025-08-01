d92dd56c95c062393773622c3f2e616158312bc1
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-08-01 12:54:36 -0400

    create open user

--- api/src/backend/queries/open_users.py
+++ api/src/backend/queries/open_users.py
@@ -17,3 +17,17 @@ async def get_open_user(conn: asyncpg.Connection, auth0_user_id: str) -> Optiona
         return None
     
     return OpenUser(**dict(result))
+
+@db_operation
+async def create_open_user(conn: asyncpg.Connection, open_user: OpenUser) -> None:
+    await conn.execute(
+        """
+            INSERT INTO open_users (open_hotkey, auth0_user_id, email, name, registered_at)
+            VALUES ($1, $2, $3, $4, $5)
+        """,
+        open_user.open_hotkey,
+        open_user.auth0_user_id,
+        open_user.email,
+        open_user.name,
+        open_user.registered_at
+    )
