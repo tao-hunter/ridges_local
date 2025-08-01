4cac3b66622559838909c2bcf2b3600a42b14db1
Jalal Bouri <jalal@hiddenharvestventures.com>
2025-08-01 13:22:22 -0400

    open user sign in endpoint

--- api/src/backend/entities.py
+++ api/src/backend/entities.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from uuid import UUID
 
 from fastapi import WebSocket
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from typing import Literal, Optional, TYPE_CHECKING
 from enum import Enum
 
@@ -459,3 +459,9 @@ class OpenUser(BaseModel):
     name: str
     registered_at: datetime
     agents: Optional[list[MinerAgent]] = []
+
+class OpenUserSignInRequest(BaseModel):
+    auth0_user_id: str
+    email: EmailStr
+    name: str
+    password: str
